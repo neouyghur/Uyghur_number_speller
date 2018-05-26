00ccebaa0f661c142aa033592e745e140d30de32
Miraç Göksu Öztürk <miracgoksuozturk@gmail.com>
2018-05-26 20:11:04 +0300

    speller is added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # turkish_number_speller
-python script to spell numbers and dates in turkish 
+python script to spell numbers and dates in turkish.
+
+## Usage
+Usage is quite simple, for numbers:
+```
+$ python speller.py -n 92
+doksan iki
+```
+for dates:
+```
+$ python speller.py -d 4.5.1992
+dört mayıs bin dokuz yüz doksan iki
+```
---
+++ speller.py
@@ -0,0 +1,96 @@
+
+import argparse
+
+
+n1 = ['', 'bir', 'iki', 'üç', 'dört', 'beş', 'altı', 'yedi', 'sekiz', 'dokuz']
+n2 = ['', 'on', 'yirmi', 'otuz', 'kırk', 'elli', 'altmış', 'yetmiş', 'seksen', 'doksan']
+
+months = ['ocak', 'şubat', 'mart', 'nisan', 'mayıs', 'haziran', 'temmuz', 'ağustos', 'eylül', 'ekim', 'kasım', 'aralık']
+month_durations = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def only_3_digits_to_str(n):
+    if n == 0:
+        return 'sıfır'
+    n_str = str(n)
+    len_s = len(n_str)
+    if len_s > 3:
+        raise ValueError('more than 3 digits')
+
+    if len_s == 1:
+        result = n1[n]
+    elif len_s == 2:
+        result = (n2[int(n_str[0])] + ' ' + n1[int(n_str[1])]).lstrip().rstrip()
+    else:
+        s2 = (n2[int(n_str[1])] + ' ' + n1[int(n_str[2])]).lstrip().rstrip()
+        if n_str[0] == '1':
+            s1 = 'yüz'
+        elif n_str[0] == '0':
+            s1 = ''
+        else:
+            s1 = n1[int(n_str[0])] + ' yüz'
+        result = (s1 + ' ' + s2).lstrip().rstrip()
+    return result
+
+
+def n_2_str(n_num):
+    n_num_s = str(n_num)
+    len_s = len(n_num_s)
+    n = len_s
+    if n > 12:
+        raise ValueError('supported up to 12 digits')
+
+    n_str = only_3_digits_to_str(int(n_num_s[-3:]))
+    if n > 3:
+        if n_str == 'sıfır':
+            n_str = ''
+        thousands = only_3_digits_to_str(int(n_num_s[-6:-3]))
+        if thousands == 'bir':
+            n_str = 'bin ' + n_str
+        elif thousands == 'sıfır':
+            pass
+        else:
+            n_str = thousands + ' bin ' + n_str
+    if n > 6:
+        millions = only_3_digits_to_str(int(n_num_s[-9:-6]))
+        if millions == 'sıfır':
+            pass
+        else:
+            n_str = millions + ' milyon ' + n_str
+    if n > 9:
+        billions = only_3_digits_to_str(int(n_num_s[-12:-9]))
+        if billions == 'sıfır':
+            pass
+        else:
+            n_str = billions + ' milyar ' + n_str
+
+    return n_str
+
+
+def d_2_str(d):
+    try:
+        day_n, month_n, year_n = map(int, d.strip().split('.'))
+    except ValueError as e:
+        print('Invalid date, please use dd.mm.yyyy')
+        exit(1)
+    assert 0 < month_n <= 12, "invalid month"
+    assert 0 < day_n <= month_durations[month_n-1], "invalid day"
+    month_str = months[month_n-1]
+
+    year_str = n_2_str(year_n)
+    date_str = n_2_str(day_n) + ' ' + month_str + ' ' + year_str
+
+    return date_str
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Number or date speller in Turkish')
+    arg_group = parser.add_mutually_exclusive_group(required=True)
+    arg_group.add_argument('-n', '--number', help='number to be spelled', type=int, dest='n')
+    arg_group.add_argument('-d', '--date', help='date to be spelled, as dd.mm.yyyy', type=str, dest='d')
+    args = parser.parse_args()
+
+    if args.n:
+        print(n_2_str(args.n))
+    else:
+        print(d_2_str(args.d))
